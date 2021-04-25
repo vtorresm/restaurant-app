b47d8d4f06804082b6a5a36fe458ce84ba045aff
Victor <vatorresm88@gmail.com>
2021-04-25 14:34:13 -0500

    Define DB Mode

---
+++ RestaurantAPI/RestaurantAPI/Models/OrderDetail.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RestaurantAPI.Models
+{
+    public class OrderDetail
+    {
+        [Key]
+        public long OrderDetailId { get; set; }
+
+        public long OrderMasterId { get; set; }
+
+        public int FoodItemId { get; set; }
+        public FoodItem FoodItem { get; set; }
+
+        public decimal FoodItemPrice { get; set; }
+
+        public int Quantity { get; set; }
+    }
+}
